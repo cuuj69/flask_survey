4de159331a9a56c437371e62c5cf6ba573c1468c
cuuj69 <mensahjefferson69@gmail.com>
2023-08-18 20:54:45 +0100

    Added:js(question_creation)

---
+++ templates/static/create_survey.js
@@ -0,0 +1,13 @@
+const questionContainer = document.querySelector('.question-container');
+const addQuestionButton = document.getElementById('add-question');
+
+addQuestionButton.addEventListener('click',()=>{
+    const newQuestion = document.createElement('div');
+    newQuestion.className = 'question';
+    newQuestion.innerHTML = `
+    <label for="question">Survey Question:</label>
+    <input type="text" name="question[]" required><br>
+    <label for="options">Options(comma-separated):</label>
+    <input type="text" name="options[]" required><br>`
+    questionContainer.appendChild(newQuestion);
+})
