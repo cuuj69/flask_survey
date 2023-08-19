e8f3f4241cc861c3e9ed318261de9f17a0806d7e
cuuj69 <mensahjefferson69@gmail.com>
2023-08-19 01:08:02 +0000

    Modified:multiple_questions

--- templates/static/create_survey.js
+++ templates/static/create_survey.js
@@ -1,13 +1,16 @@
+// static/create_survey.js
 const questionContainer = document.querySelector('.question-container');
 const addQuestionButton = document.getElementById('add-question');
 
-addQuestionButton.addEventListener('click',()=>{
+addQuestionButton.addEventListener('click', () => {
+    alert('Button clicked')
     const newQuestion = document.createElement('div');
     newQuestion.className = 'question';
     newQuestion.innerHTML = `
-    <label for="question">Survey Question:</label>
-    <input type="text" name="question[]" required><br>
-    <label for="options">Options(comma-separated):</label>
-    <input type="text" name="options[]" required><br>`
+        <label for="question">Survey Question:</label>
+        <input type="text" name="question[]" required><br>
+        <label for="options">Options (comma-separated):</label>
+        <input type="text" name="options[]" required><br>
+    `;
     questionContainer.appendChild(newQuestion);
-})
+});
